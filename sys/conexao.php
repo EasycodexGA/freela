841abf03c5584c39f06f7f1866e4190535817f28
Bernardo Fraga <fragacostabe@gmail.com>
2024-02-17 19:59:51 -0300

    .

--- sys/conexao.php
+++ sys/conexao.php
@@ -18,7 +18,7 @@ header('Access-Control-Allow-Headers: *');
 $__EMAIL__ = $_SESSION["email"];
 $__PASSWORD__ = $_SESSION["password"];
 
-$_query_ = mysqli_query($__CONEXAO__, "select * from users where email='$__EMAIL__' and password='$__PASSWORD__'");
+$_query_ = mysqli_query($__CONEXAO__, "select * from users where email='$__EMAIL__' and password='$__PASSWORD__'")or die("aa");
 
 if(mysqli_num_rows($_query_) < 1){
     session_destroy();
