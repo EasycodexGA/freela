53666fe826e45de0e7ffc5d445b7f668264287ea
Bernardo Fraga <fragacostabe@gmail.com>
2024-02-17 22:56:27 -0300

    .

--- login.php
+++ login.php
@@ -17,7 +17,7 @@ cantLog($__EMAIL__);
     <div id="outBox">
         <div class="logo">
             <div class="img-div">
-                <img src="prefeitura.png">
+                <img src="img/prefeitura.png">
             </div>
             <p class="title-p1">Voleibol</p>
             <p class="title-p2">Escolinhas</p>
