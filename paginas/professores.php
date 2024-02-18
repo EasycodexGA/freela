e0ed8af97544fb17b465dff9a43593c43b88175d
Bruno Wotzke <brunoricardowotzke@gmail.com>
2024-02-18 14:50:20 -0300

    .

--- paginas/professores.php
+++ paginas/professores.php
@@ -7,8 +7,18 @@ justLog($__EMAIL__, $__TYPE__, 2);
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../style/root.css">
+    <link rel="stylesheet" href="../style/main.css">
+    <link rel="stylesheet" href="../style/paginas.css">
+    <link rel="shortcut icon" href="../img/prefeitura.png" type="image/x-icon">
 </head>
 <body>
-    Professores
+    <header>
+        <h1 class='title-header'>Geral</h1>
+        <div class='header-in'>
+            <h2 class='sub-header'><span>0</span> Ativos</h2>
+            <h2 class='sub-header'><span>0</span> Inativos</h2>
+        </div>
+    </header>
 </body>
 </html>
