be63bdfbda7b1ef87899b74c73ea785ad626c659
Bernardo Fraga <fragacostabe@gmail.com>
2024-02-17 22:54:46 -0300

    .

--- login.php
+++ login.php
@@ -15,43 +15,30 @@ cantLog($__EMAIL__);
 </head>
 <body>
     <div id="outBox">
-        <div id="loginBox">
-            <h1 class="title-login">Entre em sua conta</h1>
-            <div id="loginTop">
-                <div class="inputDiv">
-                    <input onfocusin="focusIn(this)" onfocusout="focusOut(this)" name="email" id="email">
-                    <span class="spanlog">Email</span>
-                </div>
-                <div class="inputDiv">
-                    <input type="password" onfocusin="focusIn(this)" onfocusout="focusOut(this)" name="password" id="password">
-                    <span class="spanlog">Senha</span>
-                </div>
+        <div class="logo">
+            <div class="img-div">
+                <img src="prefeitura.png">
             </div>
-            <div id="loginBot">
-                <button onclick="this.children[0].classList.toggle('connect-active')" class='connect-bt'>
-                    <span>
-                        <svg width="38" height="27" viewBox="0 0 38 27" fill="none" xmlns="http://www.w3.org/2000/svg">
-                        <path d="M2.5 9L10.5 23L35.5 2" stroke="white" stroke-width="5"/>
-                        </svg>
-                    </span>
-                    Manter-me conectado
-                </button>
+            <p class="title-p1">Voleibol</p>
+            <p class="title-p2">Escolinhas</p>
+        </div>
+        <div id="loginBox">
+            <div class="inputDiv">
+                <p class="spanlog">Email</p>
+                <input name="email" id="email">
+            </div>
+            <div class="inputDiv">
+                <p class="spanlog">Senha</p>
+                <input type="password" name="password" id="password">
+            </div>
+            <div class="loginBot">
                 <button id="sendData">Enviar</button>
+                <button id="forgotPass">Esqueceu sua senha?</button>
             </div>
         </div>
     </div>
 
     <script>
-        function focusIn(me){
-            me.parentElement.children[1].classList.add('focusin')
-        }
-
-        function focusOut(me){
-            if(!me.value){
-                me.parentElement.children[1].classList.remove("focusin");
-            }
-        }
-
         sendData.addEventListener('click', ()=>{
             let data = {email: email.value, password: password.value}
             fetch('./sys/api/loginApi',{
