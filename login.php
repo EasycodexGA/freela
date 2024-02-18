7c1c77b7cd1168e2e5a6679866bb11ce4e5e14ea
Bruno Wotzke <brunoricardowotzke@gmail.com>
2024-02-18 02:53:48 -0300

    .

--- login.php
+++ login.php
@@ -19,7 +19,7 @@ cantLog($__EMAIL__);
     <div id="outBox">
         <div class="logo">
             <div class="img-div">
-                <img src="img/prefeitura.jpg">
+                <img src="img/prefeitura.png">
             </div>
             <p class="title-p1">Voleibol</p>
             <p class="title-p2">Escolinhas</p>
