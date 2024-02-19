e731399580231603b9e42358209eb5c556b7f259
Bernardo Fraga <fragacostabe@gmail.com>
2024-02-18 22:49:40 -0300

    .

--- sys/api/usuarios/get/categorias.php
+++ sys/api/usuarios/get/categorias.php
@@ -1,7 +1,7 @@
 <?php
 include '../../../conexao.php';
 
-justLog($__EMAIL__, $__TYPE__, 2);
+justLog($__EMAIL__, $__TYPE__, 1);
 
 $_query_ = mysqli_query($__CONEXAO__, "select * from categorias");
 
@@ -9,7 +9,7 @@ $array = array();
 
 while($dados = mysqli_fetch_array($_query_)){
     $nome = decrypt($dados["nome"]);
-    
+
     $arr = array("id"=>$dados["id"], "nome"=>$nome);
     array_push($array, $arr);
 }
