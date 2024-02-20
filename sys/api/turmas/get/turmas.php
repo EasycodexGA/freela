f7bd75d19c49bc7a7369ce76039ed75cdd3f4cc3
Bernardo Fraga <fragacostabe@gmail.com>
2024-02-20 13:24:51 -0300

    .

--- sys/api/turmas/get/turmas.php
+++ sys/api/turmas/get/turmas.php
@@ -22,7 +22,7 @@ if($__TYPE__ < 2){
         echo 'Valuedec: ' . $valuedec;
         $turmas .= $valuedec . ' , ';
     }
-    $turmas = rtrim($turmas, ' , ');
+    $turmas = substr($turmas, 0, -3);
     echo 'Turmas: ' . $turmas;
     $complemento = 'where id in ($turmas) order by field(id, $turmas)';
     $_query_ = mysqli_query($__CONEXAO__, "select * from turmas $complemento");
