61ab1c40fce492907a971628613151360f43c104
Bernardo Fraga <fragacostabe@gmail.com>
2024-02-20 18:30:16 -0300

    .

--- js/func.js
+++ js/func.js
@@ -79,7 +79,7 @@ function getCategorias(){
             `;
         }
         if(e.mensagem.length == 0){
-            tabList.innerHTML += "<tr><td></td><td>Nenhuma turma encontrada</td><td></td></tr>";
+            tabList.innerHTML += "<tr><td></td><td style='text-align: center'>Nenhum dado encontrado</td><td></td></tr>";
         }
     })
 }
@@ -103,7 +103,7 @@ function getTurmas(){
             `;
         }
         if(e.mensagem.length == 0){
-            tabList.innerHTML += "<tr><td></td><td>Nenhuma turma encontrada</td><td></td></tr>";
+            tabList.innerHTML += "<tr><td></td><td style='text-align: center'>Nenhum dado encontrado</td><td></td></tr>";
         }
     })
 }
@@ -126,7 +126,7 @@ function getAlunos(){
             `;
         }
         if(e.mensagem.length == 0){
-            tabList.innerHTML += "<tr><td></td><td>Nenhuma turma encontrada</td><td></td></tr>";
+            tabList.innerHTML += "<tr><td></td><td style='text-align: center'>Nenhum dado encontrado</td><td></td></tr>";
         }
     })
 }
@@ -149,7 +149,7 @@ function getProfessores(){
             `;
         }
         if(e.mensagem.length == 0){
-            tabList.innerHTML += "<tr><td></td><td>Nenhuma turma encontrada</td><td></td></tr>";
+            tabList.innerHTML += "<tr><td></td><td style='text-align: center'>Nenhum dado encontrado</td><td></td></tr>";
         }
     })
 }
