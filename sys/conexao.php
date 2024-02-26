5027e6cfb2c7fd3068c33d0264903a4c2782b3f7
Bernardo Fraga <fragacostabe@gmail.com>
2024-02-26 19:39:46 -0300

    .

--- sys/conexao.php
+++ sys/conexao.php
@@ -133,7 +133,7 @@ function justLog($__EMAIL__, $__TYPE__, $type){
 
 function requireLevel($__TYPE__, $type){
     if($__TYPE__ < $type or !$__TYPE__){
-        echo $__TYPE__, $type
+        echo $__TYPE__, $type;
         return false;
     }
     return true;
