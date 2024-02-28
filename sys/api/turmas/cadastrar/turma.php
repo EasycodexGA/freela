688357bf3eab8d1b0f5dd111a1383a0392790734
Bernardo Fraga <fragacostabe@gmail.com>
2024-02-27 22:19:41 -0300

    little changes

--- sys/api/turmas/cadastrar/turma.php
+++ sys/api/turmas/cadastrar/turma.php
@@ -55,8 +55,8 @@ if(mysqli_num_rows($getTurma) > 0){
     endCode("Já existe uma turma com esses dados.", false);
 }
 
-mysqli_query($__CONEXAO__, "insert into turmas (nome, categoria, horario, data) values ('$nome', '$categoria', '$horario','$__TIME__')");
-$idTurma = mysqli_insert_id($__CONEXAO__);
+$query = mysqli_query($__CONEXAO__, "insert into turmas (nome, categoria, horario, data) values ('$nome', '$categoria', '$horario','$__TIME__')");
+$idTurma = mysqli_insert_id($query);
 $idTurma = encrypt($idTurma);
 
 mysqli_query($__CONEXAO__, "insert into professores (email, turma) values ('$respEmail', '$idTurma')");
