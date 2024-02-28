ad70305b01a02caf29e363d0ecd9674036f69da7
Bernardo Fraga <fragacostabe@gmail.com>
2024-02-27 21:58:16 -0300

    bug fix

--- paginas/eventos.php
+++ paginas/eventos.php
@@ -8,7 +8,6 @@ justLog($__EMAIL__, $__TYPE__, 1);
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../style/root.css">
-    <link rel="stylesheet" href="../style/main.css">
     <link rel="stylesheet" href="../style/paginas.css">
     <link rel="shortcut icon" href="../img/prefeitura.png" type="image/x-icon">
     <script src="../js/func.js"></script>
