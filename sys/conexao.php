1dc09d3925193e64ee705942dc7410d23b71677a
Bernardo Fraga <fragacostabe@gmail.com>
2024-02-27 22:20:18 -0300

    little changes

--- sys/conexao.php
+++ sys/conexao.php
@@ -3,9 +3,6 @@ include"auth.php";
 date_default_timezone_set('America/Sao_Paulo');
 header('Content-Type: text/html; charset=utf-8');
 
-echo decrypt("sg==");
-echo encrypt("2");
-
 session_start();
 $__CONEXAO__ = mysqli_connect(
     LOG_DB_LOCAL,
