ddb0dae13031f579a17d0533eb3f5df54d5a9a6a
Bernardo Fraga <fragacostabe@gmail.com>
2024-02-29 15:27:44 -0300

    .

--- sys/api/detalhes/eventos.php
+++ sys/api/detalhes/eventos.php
@@ -44,7 +44,7 @@ while($_dados_ = mysqli_fetch_array($_query_)){
         "nome"      => decrypt($nome),
         "turmas"    => $turmas,
         "descricao" => $descricao,
-        "turmasQt"  => mysqli_num_rows($query),
+        "turmasQt"  => mysqli_num_rows($getTurmas),
         "status"    => $status,
         "created"   => $created
     );
