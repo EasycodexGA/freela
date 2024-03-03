f063562ea2eb125cb34454def72c242f2fd72ec4
Bernardo Fraga <fragacostabe@gmail.com>
2024-03-03 17:08:23 -0300

    .

--- sys/api/usuarios/cadastrar/aluno.php
+++ sys/api/usuarios/cadastrar/aluno.php
@@ -8,86 +8,84 @@ header('Content-Type: application/json; charset=utf-8');
 $request    = file_get_contents('php://input');
 $json       = json_decode($request);
 
-var_dump($json);
+$espera     = $json->espera;
 
-// $espera     = $json->espera;
-
-// if(gettype($espera) !== boolean){
-//     endCode('Erro.', false);
-// }
+if(gettype($espera) !== boolean){
+    endCode('Erro.', false);
+}
 
 
-// $cpf        = scapeString($__CONEXAO__, $json->cpf);
-// $nome       = scapeString($__CONEXAO__, $json->nome);
-// $email      = scapeString($__CONEXAO__, $json->email);
-// $nascimento = scapeString($__CONEXAO__, $json->nascimento);
+$cpf        = scapeString($__CONEXAO__, $json->cpf);
+$nome       = scapeString($__CONEXAO__, $json->nome);
+$email      = scapeString($__CONEXAO__, $json->email);
+$nascimento = scapeString($__CONEXAO__, $json->nascimento);
 
-// $cpf        = setCpf($cpf);
-// $nome       = setString($nome);
-// $email      = setEmail($email);
-// $nascimento = setNum($nascimento);
+$cpf        = setCpf($cpf);
+$nome       = setString($nome);
+$email      = setEmail($email);
+$nascimento = setNum($nascimento);
 
-// checkMissing(
-//     array(
-//         $cpf, 
-//         $nome, 
-//         $nascimento
-//     )
-// );
+checkMissing(
+    array(
+        $cpf, 
+        $nome, 
+        $nascimento
+    )
+);
 
-// if(!$email){
-//     endCode("Email inválido", false);
-// }
+if(!$email){
+    endCode("Email inválido", false);
+}
 
-// stopUserExist($__CONEXAO__, $email);
+stopUserExist($__CONEXAO__, $email);
 
-// if($espera){
-//     mysqli_query($__CONEXAO__, "insert into listaespera (nome, email, cpf, nascimento) values ('$nome', '$email', '$cpf', '$nascimento')")
-//     endCode("Sucesso! Usuário adicionado na lista de espera.", true);
-// }
+if($espera){
+    mysqli_query($__CONEXAO__, "insert into listaespera (nome, email, cpf, nascimento) values ('$nome', '$email', '$cpf', '$nascimento')")
+    endCode("Sucesso! Usuário adicionado na lista de espera.", true);
+}
 
-// $turma  = scapeString($__CONEXAO__, $json->turma);
-// $turma  = setNum($turma);
-// checkMissing(array($turma));
-// $tid = decrypt($turma);
+$turma  = scapeString($__CONEXAO__, $json->turma);
+$turma  = setNum($turma);
+checkMissing(array($turma));
+$tid = decrypt($turma);
 
-// $queryRoom = mysqli_query($__CONEXAO__, "select * from turmas where id='$tid '");
+$queryRoom = mysqli_query($__CONEXAO__, "select * from turmas where id='$tid '");
 
-// if(mysqli_num_rows($queryRoom) < 1){
-//     endCode("Turma inexistente", false);
-// }
+if(mysqli_num_rows($queryRoom) < 1){
+    endCode("Turma inexistente", false);
+}
 
-// $senha = bin2hex(random_bytes(3));
-// $senhaH = password_hash($senha, PASSWORD_DEFAULT);
+$senha = bin2hex(random_bytes(3));
+$senhaH = password_hash($senha, PASSWORD_DEFAULT);
 
-// mysqli_query($__CONEXAO__, "insert into users (nome, email, senha, cpf, nascimento, lastModify) values ('$nome', '$email', '$senhaH', '$cpf', '$nascimento', '$__TIME__')")  or die("erro insert");
-// mysqli_query($__CONEXAO__, "insert into alunos (email, turma) values ('$email', '$tid')")  or die("erro insert");
+mysqli_query($__CONEXAO__, "insert into users (nome, email, senha, cpf, nascimento, lastModify) values ('$nome', '$email', '$senhaH', '$cpf', '$nascimento', '$__TIME__')")  or die("erro insert");
+mysqli_query($__CONEXAO__, "insert into alunos (email, turma) values ('$email', '$tid')")  or die("erro insert");
 
-// $subject = "Sua senha provisória é $senha";
-// $message = "
-// <html>
-//     <head>
-//         <title>$subject</title>
-//         <style>
-//             body { font-family: Arial, sans-serif; }
-//         </style>
-//     </head>
-//     <body>
-//         <div style='background-color:#114494; color:white; text-align:center; padding: 5px; border-radius: 5px'>
-//             <h2 style='color:white;'>Senha provisória - Aluno</h2>
-//         </div>
-//         <div style='text-align:center; padding: 5px'>
-//             <p style='color:black;'>Sua senha provisória</p>
-//             <h1 style='color:black;'>$senha</h1>
-//             <p style='color:black; font-size: 13px'>Obrigado por contar conosco!</p>
-//             <p style='color:black; font-size: 12px'>Todos os direitos reservados - Easycodex $__YEAR__</p>
-//         </div>
-//     </body>
-// </html>
-// ";
+$subject = "Sua senha provisória é $senha";
+$message = "
+<html>
+    <head>
+        <title>$subject</title>
+        <style>
+            body { font-family: Arial, sans-serif; }
+        </style>
+    </head>
+    <body>
+        <div style='background-color:#114494; color:white; text-align:center; padding: 5px; border-radius: 5px'>
+            <h2 style='color:white;'>Senha provisória - Aluno</h2>
+        </div>
+        <div style='text-align:center; padding: 5px'>
+            <p style='color:black;'>Sua senha provisória</p>
+            <h1 style='color:black;'>$senha</h1>
+            <p style='color:black; font-size: 13px'>Obrigado por contar conosco!</p>
+            <p style='color:black; font-size: 12px'>Todos os direitos reservados - Easycodex $__YEAR__</p>
+        </div>
+    </body>
+</html>
+";
 
-// $sendEmail = mail(decrypt($email), $subject, $message, implode("\r\n", $__HEADERS__));
+$sendEmail = mail(decrypt($email), $subject, $message, implode("\r\n", $__HEADERS__));
 
-// $dec = decrypt($email);
+$dec = decrypt($email);
 
-// endCode("Sucesso $dec", true);
+endCode("Sucesso $dec", true);
