4e71f6e17fc1bc3fb2e05673ce4e4f608107ab1e
Bruno Wotzke <brunoricardowotzke@gmail.com>
2024-03-14 13:19:34 -0300

    galeria

--- paginas/galeria.php
+++ paginas/galeria.php
@@ -20,44 +20,49 @@ include "../sys/conexao.php";
     <div class='extra'>
         <h1 class='title-header'>Funções</h1>
         <div class='header-in'>
-            <button onclick='openAdd(addGaleria)' class='funcBt'>+ Adicionar turma</button>
-            <button onclick='' class='funcBt'>+ Novo recado</button>
+            <button onclick='openAdd(addGaleria)' class='funcBt'>+ Criar pasta</button>
+            <button onclick='' class='funcBt'>+ Enviar imagens</button>
         </div>
     </div>
 
     <div id='addNew'>
         <div id='addGaleria' class='add-container'>
-            <h1 class='title-add'>Nova turma</h1>
+            <h1 class='title-add'>Nova pasta</h1>
 
             <div class='inps-add'>
                 <div class='inp-add-out'>
                     <h3>Nome</h3>
-                    <input id='nomeAdd' type='text' placeholder='Vôlei de praia'/>
-                </div>
-                <div class='inp-add-out'>
-                    <h3>Categorias</h3>
-                    <select id='categoriaAdd'>
-                        <option value=''>Nenhuma categoria</option>
-                    </select>
-                </div>
-                <div class='inp-add-out'>
-                    <h3>Responsável</h3>
-                    <select id='profissionalAdd'>
-                        <option value=''>Nenhum profissional</option>
-                    </select>
-                </div>
-                <div class='inp-add-out'>
-                    <h3>Horário</h3>
-                    <input id='horarioAdd' type='time'/>
+                    <input id='nomeAdd' type='text' placeholder='Campeonato Estadual'/>
                 </div>
             </div>
             <div class='out-bt-sv'>
                 <button class='btn-close' onclick='closeAdd()'>Fechar</button>
-                <button onclick='addNewData("turmas/cadastrar/turma", {
-                    nome: nomeAdd.value,
-                    categoria: categoriaAdd.value,
-                    horario: horarioAdd.valueAsNumber,
-                    profissional: profissionalAdd.value,
+                <button onclick='addNewData("galeria/grupo/add", {
+                    nome: nomeAdd.value
+                })' class='btn-add'>Salvar</button>
+            </div>
+        </div>
+        <div id='addImages' class='add-container'>
+            <h1 class='title-add'>Enviar imagens</h1>
+
+            <div class='inps-add'>
+                <div class='inp-add-out'>
+                    <h3>Pasta</h3>
+                    <select id='pasta'>
+                        <option>Nenhuma pasta selecionada</option>
+                    </select>
+                </div>
+            </div>
+            <div class='inps-add'>
+                <div class='inp-add-out'>
+                    <h3>Pasta</h3>
+                    <input id='imageAdd' multiple type='file' placeholder='Nova imagem' accept="image/png, image/jpeg"/>
+                </div>
+            </div>
+            <div class='out-bt-sv'>
+                <button class='btn-close' onclick='closeAdd()'>Fechar</button>
+                <button onclick='addNewData("galeria/grupo/add", {
+                    nome: nomeAdd.value
                 })' class='btn-add'>Salvar</button>
             </div>
         </div>
