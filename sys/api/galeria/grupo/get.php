307238f861c6065eb866a8f901f090b2395c7bf1
Bruno Wotzke <brunoricardowotzke@gmail.com>
2024-03-14 21:39:42 -0300

    .

--- sys/api/galeria/grupo/get.php
+++ sys/api/galeria/grupo/get.php
@@ -11,9 +11,20 @@ while($dados = mysqli_fetch_array($getAll)){
     $id     = $dados["id"];
     $nomeGp = decrypt($dados["nome"]);
 
+    $allImgs = array();
+
+    $getImgs = mysqli_query($__CONEXAO__, "select id,img from imagensgp where id='$id'");
+
+    while($d = mysqli_fetch_array($getImgs)){
+        $idD    = $d['id'];
+        $imgD   = decrypt($d['img']);
+
+        array_push($allImgs, array("id"=>$idD, "imagem"=>$imgD));
+    }
+
     array_push(
         $nomes, 
-        array("id"=>$id, "nome"=>$nomeGp)
+        array("id"=>$id, "nome"=>$nomeGp, "imagens"=>$allImgs)
     );
 }
 
