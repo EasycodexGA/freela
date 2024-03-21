7ea5f20a370cd767573fc5fe58a0efe5d8d7de93
Bruno Wotzke <brunoricardowotzke@gmail.com>
2024-03-20 23:57:19 -0300

    .

--- js/func.js
+++ js/func.js
@@ -90,9 +90,7 @@ function verMais(me, type, titleStr){
         outBt.append(saveBt);
     }
 
-    console.log(me.dataset.id)
-
-    if(me.dataset.id.length > 1){
+    if(me.dataset.all){
         let turmasAll = me.dataset.all;
         let addBt = document.createElement("button");
         addBt.setAttribute("onclick", 'verMais(this, 1, "adicionar turma")');
