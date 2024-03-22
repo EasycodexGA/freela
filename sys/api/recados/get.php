9737f7edf3de7db8ee17fa799241fba80d919ce0
Bruno Wotzke <brunoricardowotzke@gmail.com>
2024-03-22 11:31:57 -0300

    .

--- sys/api/recados/get.php
+++ sys/api/recados/get.php
@@ -1,5 +1,5 @@
 <?php
-include '../../../conexao.php';
+include '../../conexao.php';
 
 justLog($__EMAIL__, $__TYPE__, 2);
 
@@ -8,15 +8,24 @@ $_query_ = mysqli_query($__CONEXAO__, "select * from recados where from='$__ID__
 $array = array();
 
 while($dados = mysqli_fetch_array($_query_)){
-    $nome   = decrypt($dados["nome"]);
-    $status = $dados["active"];
+    $id     = $dados["id"];
+    $title  = decrypt($dados["title"]);
+    $desc   = decrypt($dados["descricao"]);
+    $type   = $dados["type"];
+    $to     = $dados["toid"];
+    $time   = $dados["time"];
+    $active = $dados["active"];
 
-    $status = $status == '1' ? "active" : "inactive";
+    $status = $active == '1' ? "active" : "inactive";
 
     $arr = array(
-        "id"            => $decCategorias,
-        "nome"          => $nome,
-        "status"        => $status
+        "id"        => $id,
+        "title"     => $title,
+        "desc"      => $desc,
+        "type"      => $type,
+        "to"        => $to,
+        "time"      => $time,
+        "status"    => $status,
     );
     array_push($array, $arr);
 }
