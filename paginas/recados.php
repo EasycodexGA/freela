dcfcd09b24130ba6f76a83edfcc595230aff84bc
Bruno Wotzke <brunoricardowotzke@gmail.com>
2024-03-22 13:34:57 -0300

    .

--- paginas/recados.php
+++ paginas/recados.php
@@ -44,6 +44,10 @@ justLog($__EMAIL__, $__TYPE__, 2);
                     <p id='typeGet' type='text'>-- -- --</p>
                 </div>
                 <div class='inp-add-out'>
+                    <h3>Data</h3>
+                    <p id='dataGet' type='text'>-- -- --</p>
+                </div>
+                <div class='inp-add-out' style='width: calc(100%)'>
                     <h3>Descrição</h3>
                     <p id='descGet' type='text'>-- -- --</p>
                 </div>
