d0f43f1750ce8f5313805ca53b8c036648c7438b
Bernardo Fraga <fragacostabe@gmail.com>
2024-03-23 11:35:42 -0300

    .

--- sys/api/detalhes/categorias.php
+++ sys/api/detalhes/categorias.php
@@ -3,23 +3,33 @@ include '../../conexao.php';
 
 justLog($__EMAIL__, $__TYPE__, 3);
 
-$categorias  = scapeString($__CONEXAO__, $_GET['id']);
-$categorias = setNum($categorias);
-$decCategorias = decrypt($categorias);
-$_query_ = mysqli_query($__CONEXAO__, "select * from categorias where id='$decCategorias'");
+$id  = scapeString($__CONEXAO__, $_GET['id']);
+$id  = setNum($id);
+$id = decrypt($id);
+$_query_ = mysqli_query($__CONEXAO__, "select * from categorias where id='$id'");
 
 $array = array();
 
-while($dados = mysqli_fetch_array($_query_)){
-    $nome   = decrypt($dados["nome"]);
-    $status = $dados["active"];
+while($_dados_ = mysqli_fetch_array($_query_)){
+    $nome   = decrypt($_dados_["nome"]);
+    $status = $_dados_["active"];
+
+
+    $query = mysqli_query($__CONEXAO__, "select id, nome from turmas where categoria='$id'");
+    $turmas = array();
+    for($dados = mysqli_fetch_array($query)){
+        $idT = $dados['id'];
+        $nomeT = decrypt($dados['nome']);
+        array_push($turmas, array("id"=>$idT, "nome"=>$nomeT));
+    }
 
     $status = $status == '1' ? "active" : "inactive";
 
     $arr = array(
-        "id"            => $decCategorias,
-        "nome"          => $nome,
-        "status"        => $status
+        "id"        => $id,
+        "nome"      => $nome,
+        "turmas"    => $turmas,
+        "status"    => $status
     );
     array_push($array, $arr);
 }
