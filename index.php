e87301d3ad67324c163331890881b9db4e3ac7f8
Bernardo Fraga <fragacostabe@gmail.com>
2024-03-22 23:45:30 -0300

    .

--- index.php
+++ index.php
@@ -73,12 +73,6 @@ include "sys/conexao.php";
     <script>
         const btns = document.querySelectorAll("button.btn");
 
-        iframePage.addEventListener("change", {
-            if(iframePage.src == "#leave"){
-                window.location.reload();
-            }
-        })
-
         const openPage = (e, el) => {
             loading.classList.add("load-active");
 
