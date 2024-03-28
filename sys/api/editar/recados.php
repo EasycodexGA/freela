60892d41f2d6a26be40ccffdf1b659162f33b345
Bruno Wotzke <brunoricardowotzke@gmail.com>
2024-03-28 02:17:30 -0300

    .

--- sys/api/editar/recados.php
+++ sys/api/editar/recados.php
@@ -12,11 +12,13 @@ $id     = scapeString($__CONEXAO__, $json->id);
 $title  = scapeString($__CONEXAO__, $json->title);
 $desc   = scapeString($__CONEXAO__, $json->desc);
 $time   = scapeString($__CONEXAO__, $json->data);
+$active = scapeString($__CONEXAO__, $json->active);
 
 $id     = setNum($id);
 $title  = setNoXss($title);
 $desc   = setNoXss($desc);
 $time   = setNum($time);
+$active = setNum($active);
 
 checkMissing(
     array(
@@ -33,6 +35,6 @@ $time   = decrypt($time);
 // coloquei o active para ser automativo e ficar inativo depois de passar a data 
 // demais informações que não estão aqui, não precisam ser editadas 
 
-mysqli_query($__CONEXAO__, "update recados set title='$title', descricao='$desc', time='$time' where id='$id'");
+mysqli_query($__CONEXAO__, "update recados set title='$title', descricao='$desc', time='$time', active='$active' where id='$id'");
 
 endCode("Alterado com sucesso", true);
