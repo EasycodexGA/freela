a126cee832f95d216ba8af9422f6a46de9be7198
Bruno Wotzke <brunoricardowotzke@gmail.com>
2024-03-28 04:02:54 -0300

    .

--- paginas/turmas.php
+++ paginas/turmas.php
@@ -10,7 +10,8 @@ justLog($__EMAIL__, $__TYPE__, 1);
     <link rel="stylesheet" href="../style/root.css">
     <link rel="stylesheet" href="../style/paginas.css">
     <link rel="shortcut icon" href="../img/prefeitura.png" type="image/x-icon">
-    
+    <script src="../js/class.js"></script>
+    <script>const file = new Turmas(<?php echo $__TYPE__; ?>);</script>
 </head>
 <body>
     <header>
@@ -149,8 +150,6 @@ justLog($__EMAIL__, $__TYPE__, 1);
         })
     </script>
     <?php } ?>
-    <script src="../js/class.js"></script>
-    <script>const file = new Turmas(<?php echo $__TYPE__; ?>);</script>
     <script src="../js/func.js"></script>
 </body>
 </html>
