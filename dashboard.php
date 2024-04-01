227086a14d5b2f03af7c0f02bdb12039554fa2d3
Bruno Wotzke <brunoricardowotzke@gmail.com>
2024-03-31 22:52:31 -0300

    .

---
+++ dashboard.php
@@ -0,0 +1,149 @@
+<?php
+include "sys/conexao.php";
+
+justLog($__EMAIL__, $__TYPE__, 1);
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style/root.css">
+    <link rel="stylesheet" href="style/main.css">
+    <link rel="shortcut icon" href="img/prefeitura.png" type="image/x-icon">
+    <title>Voleibol escolinhas - Dashboard</title>
+    <script type="module" src="https://cdn.jsdelivr.net/npm/ldrs/dist/auto/ring2.js"></script>
+</head>
+<body>
+    <div id='recadosOut'>
+
+    </div>
+
+    <div class="int-main">
+        <div class="left">
+            <div class="left-top">
+                <div class="logo" style='background: none!important'>
+                    <div class="img-div">
+                        <img src="img/prefeitura.png">
+                    </div>
+                    <p class="title-p1">Voleibol</p>
+                    <p class="title-p2">Escolinhas</p>
+                </div>
+                <div class="links">
+                    <button onclick='openPage(`galeria`, this)' id="eventosBt" class='btn'>Galeria</button>
+                    <?php if(requireLevel($__TYPE__, 3)){ ?>
+                        <button onclick='openPage(`equipes`, this)' id="eventosBt" class='btn'>Equipes</button>
+                        <button onclick='openPage(`categorias`, this)' id="eventosBt" class='btn'>Categorias</button>
+                        <button onclick='openPage(`profissionais`, this)' id="professoresBt" class="btn">Profissionais</button>
+                    <?php } if(requireLevel($__TYPE__, 2)){ ?>
+                        <button onclick='openPage(`recados`, this)' id="alunosBt" class='btn'>Recados</button>
+                        <button onclick='openPage(`alunos`, this)' id="alunosBt" class='btn'>Alunos</button>
+                    <?php } ?>
+                        <button onclick='openPage(`turmas`, this)' id="turmasBt" class='btn'>Turmas</button>
+                        <button onclick='openPage(`eventos`, this)' id="eventosBt" class='btn'>Eventos</button>
+                        <button onclick='openPage(`configuracoes`, this)' id="configBt" class='btn'>Configurações</button>
+                        <a href='./' class='btn'>Voltar</a>
+
+
+                    <div class='patrocinadores-div'>
+                        <!-- adicionar patrocinadores aqui -->
+                    </div>
+                </div>
+            </div>
+            <div class="links"></div>
+        </div>
+        <div class="right">
+            <div id="loading">
+                <l-ring-2
+                size="40"
+                stroke="5"
+                stroke-length="0.25"
+                bg-opacity="0.1"
+                speed="0.8"
+                color="black" 
+                ></l-ring-2>
+                <!-- https://uiball.com/ldrs/ -->
+            </div>
+            <iframe src='./paginas/inicio' id='iframePage' class="right">
+            
+            </iframe>
+        </div>
+    </div>
+
+    <script>
+        const btns = document.querySelectorAll("button.btn");
+
+        iframePage.addEventListener("change", () => {
+            if(iframePage.src == "#leave"){
+                window.location.reload();
+            }
+        })
+
+        const openPage = (e, el) => {
+            loading.classList.add("load-active");
+
+            iframePage.src = `./paginas/${e}`;
+            iframePage.onload = () => {
+                for(let i of btns){
+                    i.classList.remove("link-active")
+                }
+                loading.classList.remove("load-active");
+                el.classList.add("link-active")
+            }
+        }
+
+        fetch("sys/api/recados/read")
+        .then(e=>e.json())
+        .then(e=>{
+            if(e.mensagem.length > 0 && typeof e.mensagem == "object"){
+                for(let i = 0; i < e.mensagem.length; i++){
+                    let escrita = "Fechar";
+                    let func = "closeRecados()";
+                    let startr = "none";
+
+                    if(i < e.mensagem.length - 1){
+                        escrita = "Próximo"
+                        func = "nextRecado()"
+                    }
+
+                    if(i == 0){
+                        startr = "flex"
+                    }
+
+                    recadosOut.innerHTML += `
+                        <div class='recados-in' style='display: ${startr}'>
+                            <h1 class='title-recados'>${e.mensagem[i].title}</h1>
+                            <p class='desc-recados'>${e.mensagem[i].desc}</p>
+                            <p class='desc-recados'>De: ${e.mensagem[i].from}</p>
+                            <button class='bt-recados' onclick='${func}'>${escrita}</button>
+                        </div>
+                    `
+                }
+                recadosOut.classList.add("recado-open");
+            }
+        })
+
+        function closeRecados(){
+            recadosOut.classList.remove("recado-open");
+        }
+
+        function nextRecado(){
+            recadosOut.children[0].remove();
+            recadosOut.children[0].style = "display: flex";
+        }
+
+        let intCheck = setInterval(()=>{
+            if(!localStorage.leave){
+                localStorage.leave = "false";
+            }
+            if(localStorage.leave == "true"){
+                localStorage.leave = "false";
+                setTimeout(()=>{
+                    window.location.reload();
+                },100)
+            }
+        },2000)
+    </script>
+</body>
+</html>
--- index.php
+++ index.php
@@ -9,141 +9,236 @@ include "sys/conexao.php";
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="style/root.css">
-    <link rel="stylesheet" href="style/main.css">
+    <link rel="stylesheet" href="style/landing.css">
     <link rel="shortcut icon" href="img/prefeitura.png" type="image/x-icon">
-    <title>Voleibol escolinhas - Dashboard</title>
-    <script type="module" src="https://cdn.jsdelivr.net/npm/ldrs/dist/auto/ring2.js"></script>
+    <title>Partiu vôlei - Vôleibol escolinhas</title>
+    <style>
+        *{
+            font-family: Righteous, sans-serif!important;
+        }
+        .highheader{
+            background: var(--contraste3);
+            display: flex;
+            justify-content: space-between;
+            gap: 15px;
+            padding: 5px 15px;
+        }
+        .highheader h1, .highheader span{
+            color: white;
+            font-size: 16px;
+        }
+
+        .highheader span{
+            font-size: 14px
+        }
+
+        header{
+            padding: 8px 20px;
+            display: flex;
+            align-items: center;
+            justify-content: space-between;
+            box-shadow: 0 4px 20px #00000015;
+            gap: 20px;
+        }
+        .right-h{
+            display: flex;
+            gap: 12px;
+            align-items: center;
+        }
+        .link-h {
+            color: black;
+        }
+        .login-h {
+            color: white;
+            background: var(--contraste3);
+            padding: 5px 10px;
+            border-radius: 3px;
+            font-size: 14px;
+        }
+        section#banner {
+            position: relative;
+            height: 60svh;
+            background: gray;
+            overflow: hidden;
+        }
+        #banner img{
+            width:100%;
+            height: 100%;
+            position: absolute;
+            object-fit: cover;
+        }
+
+        #black{
+            position: absolute;
+            width: 100%;
+            height: 100%;
+            background: #000000bf;
+        }
+        .container{
+            width: calc(100% - 50px);
+            padding: 15px 25px;
+            margin: 30px auto;
+            max-width: 930px;
+            display: flex;
+            flex-direction: column;
+            gap: 15px;
+        }
+        .container h1{
+            font-size: 22px;
+            color: #656565;
+        }
+
+        .container p{
+            color: #858585;
+            text-align: justify;
+        }
+        #patrocinador {
+            display: flex;
+            flex-wrap: wrap;
+            gap: 40px;
+            align-items: center;
+        }
+        #patrocinador img {
+            width: 100%;
+            max-width: 150px;
+        }
+        section#contato, section#localizacao {
+            border-radius: 5px;
+            background: var(--contraste3);
+            padding: 25px 20px 35px;
+        }
+        #contato h1, #localizacao h1{
+            color: white;
+        }
+        .infos p {
+            display: flex;
+            flex-direction: column;
+            gap: 5px;
+        }
+        form{
+            display: flex;
+            flex-wrap: wrap;
+            gap: 15px;
+        }
+        .inpout {
+            width: calc(100% / 3 - 10px);
+            display: flex;
+            flex-direction: column;
+            gap: 5px;
+        }
+        .inpout label {
+            color: white;
+        }
+        .inpout input {
+            padding: 8px 10px;
+            border-radius: 2px;
+            font-size: 13px;
+        }
+
+        form button {
+            background: var(--contraste);
+            color: white;
+            padding: 8px;
+            width: 100%;
+        }
+        #localizacao .infos p{
+            color: white;
+        }
+
+        footer{
+            margin-top: 60px;
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            padding: 30px 20px;
+            background: var(--contraste3);
+        }
+    </style>
 </head>
 <body>
-    <div id='recadosOut'>
-
-    </div>
-
-    <div class="int-main">
-        <div class="left">
-            <div class="left-top">
-                <div class="logo" style='background: none!important' onclick='openPage(`inicio`, this)'>
-                    <div class="img-div">
-                        <img src="img/prefeitura.png">
-                    </div>
-                    <p class="title-p1">Voleibol</p>
-                    <p class="title-p2">Escolinhas</p>
-                </div>
-                <div class="links">
-                    <button onclick='openPage(`galeria`, this)' id="eventosBt" class='btn'>Galeria</button>
-                    <?php if(requireLevel($__TYPE__, 3)){ ?>
-                        <button onclick='openPage(`equipes`, this)' id="eventosBt" class='btn'>Equipes</button>
-                        <button onclick='openPage(`categorias`, this)' id="eventosBt" class='btn'>Categorias</button>
-                        <button onclick='openPage(`profissionais`, this)' id="professoresBt" class="btn">Profissionais</button>
-                    <?php } if(requireLevel($__TYPE__, 2)){ ?>
-                        <button onclick='openPage(`recados`, this)' id="alunosBt" class='btn'>Recados</button>
-                        <button onclick='openPage(`alunos`, this)' id="alunosBt" class='btn'>Alunos</button>
-                    <?php } if(requireLevel($__TYPE__, 1)){ ?>
-                        <button onclick='openPage(`turmas`, this)' id="turmasBt" class='btn'>Turmas</button>
-                        <button onclick='openPage(`eventos`, this)' id="eventosBt" class='btn'>Eventos</button>
-                        <button onclick='openPage(`configuracoes`, this)' id="configBt" class='btn'>Configurações</button>
-                    <?php } else { ?>
-                        <button onclick="location.href='login'" class='btn'>Login</button>
-                    <?php } ?>
-
-                    <div class='patrocinadores-div'>
-                        <!-- adicionar patrocinadores aqui -->
-                    </div>
-                </div>
-            </div>
-            <div class="links"></div>
+    <nav class='highheader'>
+        <h1>O esporte em você</h1>
+        <span>GRATUITO</span>
+        <h1>Entre em contato ➔</h1>
+    </nav>
+    <header>
+        <div class='left-h'>
+            <img src='./img/logo.png'>
         </div>
-        <div class="right">
-            <div id="loading">
-                <l-ring-2
-                size="40"
-                stroke="5"
-                stroke-length="0.25"
-                bg-opacity="0.1"
-                speed="0.8"
-                color="black" 
-                ></l-ring-2>
-                <!-- https://uiball.com/ldrs/ -->
+        <div class='right-h'>
+            <a href='./' class='link-h'>Início</a>
+            <a href='#sobre' class='link-h'>Sobre</a>
+            <a href='#patrocinadores' class='link-h'>Patrocinadores</a>
+            <a href='#contato' class='link-h'>Contato</a>
+            <a href='#localizacao' class='link-h'>Local</a>
+            <a href='./login' class='link-h login-h'>Entrar</a>
+        </div>
+    </header>
+    <section id='banner'>
+        <img src='./img/time.png'>
+        <div id='black'></div>
+    </section>
+    <section id='sobre' class='container'>
+        <h1>Sobre o projeto</h1>
+        <p>Projeto desportivo que mira, além da inclusão social, o rendimento desportivo individual e coletivo. Em sua essência, o PROJETO POMERODE VOLEIBOL 2024, visa oportunizar às crianças e jovens do município de Pomerode o contato com a modalidade VOLEIBOL de forma gratuita e em um ambiente saudável e integrado com a sociedade.</p>
+    </section>
+    <section id='patrocinadores' class='container'>
+        <h1>Patrocinadores</h1>
+        <div id='patrocinador'>
+            <img src="https://partiuvolei.com/imagens/patrocinadores/i17104004235d47ee.png">
+            <img src="https://partiuvolei.com/imagens/patrocinadores/i17104004235d47ee.png">
+        </div>
+    </section>
+    <section id='contato' class='container'>
+        <h1>Contato</h1>
+        <form action='javascript:void(0)' class='contato'>
+            <div class='inpout'>
+                <label for="nome">Nome</label>
+                <input id='nome' type='text' placeholder='Fulano ciclano'>
             </div>
-            <iframe src='./paginas/inicio' id='iframePage' class="right">
-            
-            </iframe>
+            <div class='inpout'>
+                <label for="email">Email</label>
+                <input id='email' type='text' placeholder='exemplo@gmail.com'>
+            </div>
+            <div class='inpout'>
+                <label for="telefone">Telefone</label>
+                <input id='telefone' type='text' placeholder='47 9 9999-9999'>
+            </div>
+            <button>Enviar</button>
+        </form>
+    </section>
+    <section id='responsavel' class='container'>
+        <h1>Responsável</h1>
+        <div style="display:flex; gap: 30px; align-items: center;">
+        <div style="width: 125px; aspect-ratio: 1; position: relative; border-radius: 100px; overflow: hidden;">
+            <img style="position: absolute; width: 100%; height: 100%; object-fit: cover;" src="../img/luciano.jpg">
+        </div>
+        <div class="infos">
+            <h1 class="title-header">Contato</h1>
+            <p>
+                <span>Prof. Luciano Menegaz</span>
+                <span>F. (48) 99806 0667</span>
+                <span>lucianor.menegaz@gmail.com</span>
+            </p>
         </div>
     </div>
+    </section>
+    <section id='localizacao' class='container'>
+        <h1>Localização</h1>
+        <div class="infos">
+            <p>
+                <span>Ginasio Ralf Knaesel</span>
+                <span>Secretaria de Eventos, Esporte e Lazer</span>
+                <span>Endereço: Av. 21 de Janeiro - 2700</span>
+                <span>Pomerode - SC</span>
+                <span>Telefone(s): (47) 3387-2612</span>
+                <span>E-mail: seel@pomerode.sc.gov.br</span>
+            </p>
+        </div>
+    </section>
 
-    <script>
-        const btns = document.querySelectorAll("button.btn");
-
-        iframePage.addEventListener("change", () => {
-            if(iframePage.src == "#leave"){
-                window.location.reload();
-            }
-        })
-
-        const openPage = (e, el) => {
-            loading.classList.add("load-active");
-
-            iframePage.src = `./paginas/${e}`;
-            iframePage.onload = () => {
-                for(let i of btns){
-                    i.classList.remove("link-active")
-                }
-                loading.classList.remove("load-active");
-                el.classList.add("link-active")
-            }
-        }
-
-        fetch("sys/api/recados/read")
-        .then(e=>e.json())
-        .then(e=>{
-            if(e.mensagem.length > 0 && typeof e.mensagem == "object"){
-                for(let i = 0; i < e.mensagem.length; i++){
-                    let escrita = "Fechar";
-                    let func = "closeRecados()";
-                    let startr = "none";
-
-                    if(i < e.mensagem.length - 1){
-                        escrita = "Próximo"
-                        func = "nextRecado()"
-                    }
-
-                    if(i == 0){
-                        startr = "flex"
-                    }
-
-                    recadosOut.innerHTML += `
-                        <div class='recados-in' style='display: ${startr}'>
-                            <h1 class='title-recados'>${e.mensagem[i].title}</h1>
-                            <p class='desc-recados'>${e.mensagem[i].desc}</p>
-                            <p class='desc-recados'>De: ${e.mensagem[i].from}</p>
-                            <button class='bt-recados' onclick='${func}'>${escrita}</button>
-                        </div>
-                    `
-                }
-                recadosOut.classList.add("recado-open");
-            }
-        })
-
-        function closeRecados(){
-            recadosOut.classList.remove("recado-open");
-        }
-
-        function nextRecado(){
-            recadosOut.children[0].remove();
-            recadosOut.children[0].style = "display: flex";
-        }
-
-        let intCheck = setInterval(()=>{
-            if(!localStorage.leave){
-                localStorage.leave = "false";
-            }
-            if(localStorage.leave == "true"){
-                localStorage.leave = "false";
-                setTimeout(()=>{
-                    window.location.reload();
-                },100)
-            }
-        },2000)
-    </script>
+    <footer>
+        <img src='./img/logo2.png'>
+    </footer>
 </body>
 </html>
