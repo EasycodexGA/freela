c14bc02112c4919bea3d8b5452d8666e49b3632a
Bernardo Fraga <fragacostabe@gmail.com>
2024-04-02 22:08:49 -0300

    .

--- js/class.js
+++ js/class.js
@@ -380,8 +380,6 @@ class File{
                         tr.appendChild(td);
                     }
                 }
-                tabList2.appendChild(tr)
-
                 if(!--iterations){
                     let preStatus = value == 'active' ? true : false;
                     tr.dataset.status = preStatus;
@@ -389,6 +387,7 @@ class File{
                     td2.innerHTML = `<button class="ver-detalhes" onclick="sendEspera(${i.id})">Aprovar</button>`;
                     tr.appendChild(td2);
                 }
+                tabList2.appendChild(tr)
             }
             tabList2.innerHTML += "<tr class='empty-line table-line2' id='notData2'><td></td><td style='text-align: center'>Nenhum dado encontrado</td><td></td></tr>";
 
