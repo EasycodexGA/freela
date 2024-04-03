8b5fafd009577113ef2dc67886a466cc9db93133
Bernardo Fraga <fragacostabe@gmail.com>
2024-04-02 21:04:09 -0300

    .

--- js/class.js
+++ js/class.js
@@ -358,7 +358,7 @@ class File{
 
     createEspera(){
         console.log("Get Espera: " + this.name)
-        let link = '../sys/api/usuarios/getEspera?type=' + this.name
+        let link = '../sys/api/usuarios/getEspera.php?type=' + this.name
         return fetch(`${link}`)
         .then(e=>e.json())
         .then(e=>{ 
