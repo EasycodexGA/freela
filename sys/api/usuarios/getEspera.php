3c0f322387c3e5da92a1bfe7f28d651ce9bae490
Bernardo Fraga <fragacostabe@gmail.com>
2024-04-02 21:50:47 -0300

    .

--- sys/api/usuarios/getEspera.php
+++ sys/api/usuarios/getEspera.php
@@ -20,7 +20,7 @@ $array = array();
 while($_dados_ = mysqli_fetch_array($_query_)){
     $id         = $_dados_['id'];
     $nome       = decrypt($_dados_["nome"]);
-    // $email      = decrpyt($_dados_['email']);
+    $email      = decrpyt($_dados_['email']);
     // $nascimento = decrpy($_dados_['nascimento']);
     
     // $arr = array(
