99d851beef8384fc0395af946f9a13fd128fc4fd
Bernardo Fraga <fragacostabe@gmail.com>
2024-04-05 14:22:02 -0300

    .

--- sys/api/editar/alunos.php
+++ sys/api/editar/alunos.php
@@ -88,6 +88,7 @@ for($i = 0; $i < count($equipes); $i++){
     $check_query = mysqli_query($__CONEXAO__, "select id from alunos where equipe='$idEquipe' and email='$emm'");
     if($check){
         if(mysqli_num_rows($check_query) == 0){
+            endCode("aa", false);
             mysqli_query($__CONEXAO__, "insert into alunos (email, equipe) values ('$emm','$idEquipe')");
         }
     } else {
