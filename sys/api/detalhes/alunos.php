7c7568fe1eeef056797c60f866dfaa91819d5c3e
Bernardo Fraga <fragacostabe@gmail.com>
2024-04-05 14:55:10 -0300

    .

--- sys/api/detalhes/alunos.php
+++ sys/api/detalhes/alunos.php
@@ -56,7 +56,8 @@ while($_dados_ = mysqli_fetch_array($_query_)){
 
     $turmas = array_merge($arrTurmas, $allTurmas);
 
-    $query4  = mysqli_query($__CONEXAO__, "select id, nome from equipes where id in (select equipe from alunos where email='$email')");
+    // $query4  = mysqli_query($__CONEXAO__, "select id, nome from equipes where id in (select equipe from alunos where email='$email')");
+    $query4  = mysqli_query($__CONEXAO__, "select equipe from alunos where email='$email'");
     endCode(mysqli_num_rows($query4), false);
     $arrEquipe = array();
 
