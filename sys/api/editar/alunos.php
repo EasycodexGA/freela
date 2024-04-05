5543fc96074205324e2a408d0659f54987726494
Bernardo Fraga <fragacostabe@gmail.com>
2024-04-05 14:38:10 -0300

    .

--- sys/api/editar/alunos.php
+++ sys/api/editar/alunos.php
@@ -85,9 +85,7 @@ for($i = 0; $i < count($turmas); $i++){
 for($i = 0; $i < count($equipes); $i++){
     $check = $equipes[$i]->checked;
     $idEquipe = $equipes[$i]->id;
-    // endCode($idEquipe, false);
     $check_query = mysqli_query($__CONEXAO__, "select id from alunos where equipe='$idEquipe' and email='$emm'");
-    endCode(mysqli_num_rows($check_query), false);
     if($check){
         endCode("bb", false);
         if(mysqli_num_rows($check_query) == 0){
