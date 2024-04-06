a7c3f087c0f5f3f0bdd8aca1a9006d282bc6fa6d
Bernardo Fraga <fragacostabe@gmail.com>
2024-04-06 01:25:29 -0300

    .

--- sys/api/editar/turmas.php
+++ sys/api/editar/turmas.php
@@ -52,19 +52,17 @@ if(mysqli_num_rows($checkExist) > 0){
 }
 
 
-if($aulas !== false){
-    for($i = 0; $i < count($aulas); $i++){
-        // endCode(var_dump($aulas), false);
-        $idAula = $aulas[$i]->id;
-        $chamadaAula = $aulas[$i]->chamada;
-        for($j = 0; $j < count($chamadaAula); $j++){
-            $checkAula = $chamadaAula[$j]->checked;
-            $check_query = mysqli_query($__CONEXAO__, "select id from chamada where id='$idAula'") or endCode("asd 1", false);
-            if(mysqli_num_rows($check_query) > 0){
-                $checkChamada = mysqli_query($__CONEXAO__, "select id from chamada where id='$idAula' and presenca='$checkAula'")  or endCode("asd 2", false);
-                if(mysqli_num_rows($checkChamada) == 0){
-                    mysqli_query($__CONEXAO__, "update chamada set presenca='$checkAula' where id='$idAula'")  or endCode("asd 3", false);
-                }
+for($i = 0; $i < count($aulas); $i++){
+    // endCode(var_dump($aulas), false);
+    $idAula = $aulas[$i]->id;
+    $chamadaAula = $aulas[$i]->chamada;
+    for($j = 0; $j < count($chamadaAula); $j++){
+        $checkAula = $chamadaAula[$j]->checked;
+        $check_query = mysqli_query($__CONEXAO__, "select id from chamada where id='$idAula'") or endCode("asd 1", false);
+        if(mysqli_num_rows($check_query) > 0){
+            $checkChamada = mysqli_query($__CONEXAO__, "select id from chamada where id='$idAula' and presenca='$checkAula'")  or endCode("asd 2", false);
+            if(mysqli_num_rows($checkChamada) == 0){
+                mysqli_query($__CONEXAO__, "update chamada set presenca='$checkAula' where id='$idAula'")  or endCode("asd 3", false);
             }
         }
     }
