fb3e3e77e210d97d0b9c90f335e3efc0b0c9840e
Bruno Wotzke <brunoricardowotzke@gmail.com>
2024-04-07 20:54:28 -0300

    .

--- sys/api/recados/read.php
+++ sys/api/recados/read.php
@@ -6,7 +6,7 @@ justLog($__EMAIL__, $__TYPE__, 1);
 $todosRecados = array();
 
 $temp = $__TIME__ - 8600;
-$getRecados = $__TYPE__ == 1 ? mysqli_query($__CONEXAO__, "select * from recados where time>'$temp' and type='1' and toid='$__ID__' or time>'$temp' and type='2' and toid in (select id from turmas where id in (select turma from alunos where email='$__EMAIL__')) or time>'$temp' and type='4' and toid in (select id from turmas where id in (select equipes from alunos where email='$__EMAIL__')) or time>'$temp' and type='3' and toid='0'") : "";
+$getRecados = $__TYPE__ == 1 ? mysqli_query($__CONEXAO__, "select * from recados where time>'$temp' and type='1' and toid='$__ID__' or time>'$temp' and type='2' and toid in (select id from turmas where id in (select turma from alunos where email='$__EMAIL__')) or time>'$temp' and type='4' and toid in (select id from equipes where id in (select equipes from alunos where email='$__EMAIL__')) or time>'$temp' and type='3' and toid='0'") : "";
 
 while($dados = mysqli_fetch_array($getRecados)){
     $id  = $dados["id"];
