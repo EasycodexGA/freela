a1642c47a09f251f8151810bee9d1e9764cd2321
Bruno Wotzke <brunoricardowotzke@gmail.com>
2024-04-07 21:49:38 -0300

    .

--- sys/api/editar/turmas.php
+++ sys/api/editar/turmas.php
@@ -39,7 +39,7 @@ $active = decrypt($active);
 
 if($__TYPE__ == 2){
     $checkTurma= mysqli_query($__CONEXAO__, "select id from turmas where id in (select turma from professores where email='$__EMAIL__')") or die("b");
-    if(mysqli_num_rows($checkTurma) > 0){
+    if(mysqli_num_rows($checkTurma) < 1){
         endCode("Essa turma não lhe pertence", false);
     }
 }
