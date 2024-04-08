35c243d377fe3cea6eb4a4757f712b07f50abf4b
Bruno Wotzke <brunoricardowotzke@gmail.com>
2024-04-07 23:04:33 -0300

    .

--- sys/api/equipes/get/equipes.php
+++ sys/api/equipes/get/equipes.php
@@ -17,24 +17,16 @@ $array = array();
 while($dados = mysqli_fetch_array($_query_)){
     $id     = $dados["id"];
     $nome   = decrypt($dados["nome"]);
-    $catId  = $dados["categoria"];  
-
-    $getCat     = mysqli_query($__CONEXAO__, "select nome from categorias where id='$catId'");
-    $categoria  = mysqli_fetch_assoc($getCat)["nome"];
-    $categoria  = decrypt($categoria);
-
+    
     $status = $dados["active"];
 
     $status = $status == '1' ? "active" : "inactive";
 
-    $query = mysqli_query($__CONEXAO__, "select id from alunos where turma='$id'");
-    $query2 = mysqli_query($__CONEXAO__, "select id from professores where turma='$id'");
+    $query = mysqli_query($__CONEXAO__, "select id from alunos where equipe='$id' and email in (select email from users where active='1' and typeC='1')");
 
     $arr = array(
         "id"            => $id, 
         "nome"          => $nome, 
-        "categoria"     => $categoria,
-        "profissionais" => mysqli_num_rows($query2),
         "alunos"        => mysqli_num_rows($query),
         "status"        => $status,
         "_name"         => "turmas"
