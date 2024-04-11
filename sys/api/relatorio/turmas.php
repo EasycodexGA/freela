f803c6bdc38bd37cd5a71ffdbf321af15d34143d
Bruno Wotzke <brunoricardowotzke@gmail.com>
2024-04-11 01:32:31 -0300

    .

--- sys/api/relatorio/turmas.php
+++ sys/api/relatorio/turmas.php
@@ -4,7 +4,7 @@ include '../../conexao.php';
 justLog($__EMAIL__, $__TYPE__, 3);
 
 header('Content-Type: text/csv; charset=utf-8');
-header('Content-Disposition: attachment; filename=relatorio_aluno.csv');
+header('Content-Disposition: attachment; filename=relatorio_turmas.csv');
 
 $resultado = fopen("php://output", 'w');
 
