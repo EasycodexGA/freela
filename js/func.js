d05fdd5460276f0872c74c7641054a30fd1f9180
Bernardo Fraga <fragacostabe@gmail.com>
2024-04-11 23:08:20 -0300

    .

--- js/func.js
+++ js/func.js
@@ -119,6 +119,8 @@ function verMais(id,type, titleStr){
                         check = index
                     }
                 });
+                console.log(check)
+                console.log(i.chamada)
                 presenca.innerText = i.chamada[check].checked == 1 ? 'presente' : 'ausente';
                 div.append(presenca)
             }
