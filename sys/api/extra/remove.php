65bf72129facc72f4e024e5274b5fae6453df310
Bernardo Fraga <fragacostabe@gmail.com>
2024-04-11 22:30:09 -0300

    .

--- sys/api/extra/remove.php
+++ sys/api/extra/remove.php
@@ -4,13 +4,17 @@ include '../../conexao.php';
 justLog($__EMAIL__, $__TYPE__, 2);
 
 $id = scapeString($__CONEXAO__, $_GET['id']);
-$id = setNum($id);
-$id = decrypt($id);
 
 $local  = scapeString($__CONEXAO__, $_GET['local']);
 $local  = setString($local);
 $local  = decrypt($local);
 
+if($local == "recados"){
+    deletarRecados($__CONEXAO__, $__TYPE__, $__EMAIL__, $id);
+}
+
+$id = setNum($id);
+$id = decrypt($id);
 
 if($local == "alunos"){
     deletarAluno($__CONEXAO__, $__TYPE__, $__EMAIL__, $id);
@@ -34,15 +38,6 @@ if($local == "profissionais"){
 
 if($local == "turmas"){
     deletarTurma($__CONEXAO__, $__TYPE__, $__EMAIL__, $id);
-}
-
-
-if($local == "recados"){
-    deletarRecados($__CONEXAO__, $__TYPE__, $__EMAIL__, $id);
-}
-
-if($local == "espera"){
-    deletarEspera($__CONEXAO__, $__TYPE__, $__EMAIL__, $id);
 }
 
 
