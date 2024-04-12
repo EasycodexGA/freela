5d724353bcb19c15afbbbdbcc24bd14e126ef6b5
Bernardo Fraga <fragacostabe@gmail.com>
2024-04-11 22:22:36 -0300

    .

--- sys/api/extra/remove.php
+++ sys/api/extra/remove.php
@@ -138,7 +138,7 @@ function deletarRecados($__CONEXAO__, $__TYPE__, $__EMAIL__, $id){
 }
 
 function deletarEspera($__CONEXAO__, $__TYPE__, $__EMAIL__, $email){
-    $email = encrypt($email);
+    $email = setEmail($email);
 
     $checkQuery = mysqli_query($__CONEXAO__, "select id from listaespera where email='$email'") or die("a");
     checkQuery($__TYPE__, 'Usuário na fila de espera não encontrado.', $checkQuery, false);
