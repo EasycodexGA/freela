df254e301ee5756de3ab55969a54091a736669a7
Bruno Wotzke <brunoricardowotzke@gmail.com>
2024-04-23 13:48:51 -0300

    .

--- sys/api/usuarios/get/alunos.php
+++ sys/api/usuarios/get/alunos.php
@@ -4,9 +4,9 @@ include '../../../conexao.php';
 justLog($__EMAIL__, $__TYPE__, 2);
 
 if($__TYPE__ == 3){
-    $_query_ = mysqli_query($__CONEXAO__, "select nome, active, email, nascimento, id from users where typeC='1'");
+    $_query_ = mysqli_query($__CONEXAO__, "select nome, active, email, nascimento, id from users where typeC='1' order by id desc");
 } else {
-    $_query_ = mysqli_query($__CONEXAO__, "select nome, active, email, nascimento, id from users where typeC='1' and email in (select email from alunos where turma in (select turma from professores where email='$__EMAIL__'))");
+    $_query_ = mysqli_query($__CONEXAO__, "select nome, active, email, nascimento, id from users where typeC='1' and email in (select email from alunos where turma in (select turma from professores where email='$__EMAIL__')) order by id desc");
 }
 
 $array = array();
