0e35cbdcfac329e98150ecfca8bea8b15f9dd2b6
Bernardo Fraga <fragacostabe@gmail.com>
2024-05-15 20:24:47 -0300

    start rifa system

--- paginas/rifas.php
+++ paginas/rifas.php
@@ -13,7 +13,79 @@ justLog($__EMAIL__, $__TYPE__, 3);
     <script src="../js/func.js"></script>
 </head>
 <body>
-    <h1 class='title-header'>Rifas</h1>
+<div class='bodyin'>
+        <div class='header'>
+            <h1 class='title-header'>Geral - Rifas</h1>
+            <div class='header-in'>
+                <h2 class='sub-header'><span id="active">0</span> Ativos</h2>
+            </div>
+        </div>
+
+        <div id='details'>
+            <div class='add-container' id='detailContainer'></div>
+        </div>
+
+        <div class='extra'>
+            <h1 class='title-header'>Funções</h1>
+            <div id='headerIn' class='header-in'>
+                <button onclick='openAdd(addRifa)' class='funcBt'>+ Adicionar rifa</button>
+            </div>
+        </div>
+        
+        <div id='addNew'>
+            <div id='addRifa' class='add-container'>
+                <h1 class='title-add'>Nova rifa</h1>
+
+                <div class='inps-add'>
+                    <div class='inp-add-out'>
+                        <h3>Nome</h3>
+                        <input id='nomeAdd' type='text' placeholder='Rifa para caridade'/>
+                    </div>
+                    <div class='inp-add-out'>
+                        <h3>Descrição</h3>
+                        <input id='descAdd' type='text' placeholder='Estamos fazendo esta rifa para...'/>
+                    </div>
+                    <div class='inp-add-out'>
+                        <h3>Data de término</h3>
+                        <input id='dataAdd' type='date'/>
+                    </div>
+                    <div class='inp-add-out'>
+                        <h3>Quantidade de números</h3>
+                        <input id='qtAdd' type='number' placeholder='60'/>
+                    </div>
+                    <div class='inp-add-out2'>
+                        <h3>Limite de números?</h3>
+                        <input id='limiteNumRifa' type='checkbox'>
+                    </div>
+                    <div class='inp-add-out'>
+                        <h3>Prêmios</h3>
+                        <input id='premioAdd0' data-index='0' type='text' placeholder=''/>
+                        <!-- criar um script para gerar mais um input caso tiver algo digitado no anterior -->
+                    </div>
+                </div>
+                <div class='out-bt-sv'>
+                    <button class='btn-close' onclick='closeAdd()'>Fechar</button>
+                    <button onclick='addNewData("turmas/cadastrar/rifa", {
+                        nome: nomeAdd.value,
+                        desc: descAdd.value,
+                        data: (dataAdd.valueAsNumber / 1000),
+                        qt: getRifaQt(), //fazer as duas funções que retornam os valores -> valor normal se tiver quantidade e -1 se limiteNumRifa estiver clickado
+                        premios: getRifaPremio() // retornar um objeto com com todos os premios e suas respectivas imagens
+                    })' class='btn-add'>Salvar</button>
+                </div>
+            </div>
+        </div>
+
+        <div class="list">
+            <div class="header-list-out">
+                <h1 class="title-header">Alunos</h1>
+                <input id="searchBar" name="searchBar" placeholder="Pesquisar..">
+            </div>
+            <table class="content-list">
+                <thead id='headList'></thead>
+                <tbody id='tabList'></tbody>
+            </table>
+        </div>
     
 
     <div id="b2xcodeOut">
