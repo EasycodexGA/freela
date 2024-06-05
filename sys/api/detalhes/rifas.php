0adf8d16f7928c659a65901b9e289e5383579401
Bruno Wotzke <brunoricardowotzke@gmail.com>
2024-06-05 15:12:23 -0300

    .

--- sys/api/detalhes/rifas.php
+++ sys/api/detalhes/rifas.php
@@ -10,36 +10,36 @@ $_query_ = mysqli_query($__CONEXAO__, "select * from rifas where id='$id'");
 
 $array = array();
 
-while($_dados_ = mysqli_fetch_array($_query_)){
-    $qt         = decrypt($_dados_["qt"]);
-    $nome       = decrypt($_dados_["nome"]);
-    $desc       = decrypt($_dados_["desc"]);
-    $valor      = decrypt($_dados_["valor"]);
-    $premios    = $_dados_["premios"];
+// while($_dados_ = mysqli_fetch_array($_query_)){
+//     $qt         = decrypt($_dados_["qt"]);
+//     $nome       = decrypt($_dados_["nome"]);
+//     $desc       = decrypt($_dados_["desc"]);
+//     $valor      = decrypt($_dados_["valor"]);
+//     $premios    = $_dados_["premios"];
 
-    foreach($premios as &$i){
-        $i->nome = decrypt($i->nome);
-    }
+//     foreach($premios as &$i){
+//         $i->nome = decrypt($i->nome);
+//     }
 
-    $nums = array()
+//     $nums = array()
 
-    $query = mysqli_query($__CONEXAO__, "select num from numerorifa where ref='$id' order by asc");
-    while($dados = mysqli_fetch_array($query)){
-        $num = $dados['num'];
-        array_push($nums, $num);
-    }
+//     $query = mysqli_query($__CONEXAO__, "select num from numerorifa where ref='$id' order by asc");
+//     while($dados = mysqli_fetch_array($query)){
+//         $num = $dados['num'];
+//         array_push($nums, $num);
+//     }
 
 
-    $arr = array(
-        "id"        => $id,
-        "nome"      => $nome,
-        "desc"      => $desc,
-        "premios"   => $premios,
-        "qt"        => $qt,
-        "valor"     => $valor,
-        "selected"  => $nums
-    );
-    array_push($array, $arr);
-}
+//     $arr = array(
+//         "id"        => $id,
+//         "nome"      => $nome,
+//         "desc"      => $desc,
+//         "premios"   => $premios,
+//         "qt"        => $qt,
+//         "valor"     => $valor,
+//         "selected"  => $nums
+//     );
+//     array_push($array, $arr);
+// }
 
-endCode($array, true);
+// endCode($array, true);
